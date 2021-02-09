5d21db2fd43ae3807cf51b4ff0bdcaefd4901da8
Leonardo Buck de Godoy <leonardo@timais.com>
2021-02-09 13:06:55 -0300

    first commit

---
+++ BookStore/Controllers/TesteController.cs
@@ -0,0 +1,47 @@
+﻿using BookStore.Domain;
+using System.Web.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class TesteController : Controller
+    {
+        public ViewResult Dados()
+        {
+            var autor = new Autor
+            {
+                Id = 1,
+                Nome = "Leo",
+            };
+
+            ViewBag.Categoria = "Produtos de Limpeza";
+            ViewData["Categoria"] = "Produtos de Informática";
+            TempData["Categoria"] = "Produtos de Escritório";
+            Session["Categoria"] = "Produtos de Imóveis";
+
+            return View(autor);
+        }
+
+        public string Index(int Id)
+        {
+            return "Index do " + Id.ToString();
+        }
+
+        public JsonResult UmaAction(int? id, string nome)
+        {
+            var autor = new Autor
+            {
+                Id = 0,
+                Nome = nome,
+            };
+
+            return Json(autor, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        [ActionName("Autor")]
+        public JsonResult ActionDois(Autor autor)
+        {
+            return Json(autor);
+        }
+    }
+}
