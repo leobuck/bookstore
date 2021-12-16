22a1691f081c692857b7f851cbf141ebf5d8dbc9
leobuck <leobuck12@gmail.com>
2021-12-15 21:12:17 -0300

    Trabalhando com PartialViews

--- BookStore/Controllers/LivroController.cs
+++ BookStore/Controllers/LivroController.cs
@@ -1,7 +1,9 @@
 ﻿using BookStore.Context;
+using BookStore.Domain;
 using BookStore.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,11 +15,17 @@ namespace BookStore.Controllers
     {
         BookStoreDataContext _db = new BookStoreDataContext();
 
+        [Route("listar")]
+        public ActionResult Index()
+        {
+            return View(_db.Livros.ToList());
+        }
+
         [Route("criar")]
         public ActionResult Create()
         {
             var categorias = _db.Categorias.ToList();
-            var model = new CreateBookViewModel
+            var model = new EditorBookViewModel
             {
                 Nome = "",
                 ISBN = "",
@@ -30,9 +38,44 @@ namespace BookStore.Controllers
 
         [Route("criar")]
         [HttpPost]
-        public ActionResult Create(CreateBookViewModel model)
+        public ActionResult Create(EditorBookViewModel model)
         {
-            return View();
+            var livro = new Livro();
+            livro.Nome = model.Nome;
+            livro.ISBN = model.ISBN;
+            livro.DataLancamento = model.DataLancamento;
+            livro.CategoriaId = model.CategoriaId;
+            _db.Livros.Add(livro);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [Route("editar")]
+        public ActionResult Edit(int id)
+        {
+            var categorias = _db.Categorias.ToList();
+            var livro = _db.Livros.Find(id);
+            var model = new EditorBookViewModel
+            {
+                Nome = livro.Nome,
+                ISBN = livro.ISBN,
+                CategoriaId = livro.CategoriaId,
+                CategoriaOptions = new SelectList(categorias, "Id", "Nome")
+            };
+
+            return View(model);
+        }
+
+        [Route("editar")]
+        [HttpPost]
+        public ActionResult Edit(EditorBookViewModel model)
+        {
+            var livro = _db.Livros.Find(model.Id);
+            _db.Entry<Livro>(livro).State = EntityState.Modified;
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
